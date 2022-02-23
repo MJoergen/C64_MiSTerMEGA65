a8853be06c2f01ff0297cc4da89979c0f2e5ac49
Michael Jørgensen <mfj@silicom.dk>
2022-02-23 13:57:44 +0100

    Convert C1541 and C1581 roms from Intel format.

--- rtl/roms/convert_mif_to_hex.sh
+++ rtl/roms/convert_mif_to_hex.sh
@@ -1,5 +1,5 @@
 MIF2HEX=/opt/altera/21.1/quartus/bin/mif2hex
-for i in *.mif; do \
+for i in *.mif ../iec_drive/*.mif; do \
    echo "Processing" $i; \
    $MIF2HEX $i tmp.tmp; \
    cat tmp.tmp | cut -c 10-11 | head -n -1 > $i.hex; \
